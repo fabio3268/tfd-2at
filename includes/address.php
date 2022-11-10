1ab6f4c92ee11bd7e6b7de8d3d34ecb670c36e4c
Fábio Santos <fabio3268@gmail.com>
2022-11-10 17:05:56 -0300

    Initial commit

---
+++ includes/address.php
@@ -0,0 +1,20 @@
+<div class="header-top-bar">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <ul class="top-bar-info list-inline-item pl-0 mb-0">
+                    <li class="list-inline-item"><a href="mailto:ch-contato@ifsul.edu.br"><i class="icofont-support-faq mr-2"></i>ch-contato@ifsul.edu.br</a></li>
+                    <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>General Balbão, 81 - Charqueadas / RS </li>
+                </ul>
+            </div>
+            <div class="col-lg-6">
+                <div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+                    <a href="tel:+51-3658-3603" >
+                        <span>Liga Já : </span>
+                        <span class="h4">(51) 3658-3603</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
